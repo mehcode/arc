878107e1eec4fdfaddb96883a3f3479120521b42
Ryan Leckey <leckey.ryan@gmail.com>
2018-08-16 16:51:31 -0700

    Update fib example to compile and run with recent changes

--- examples/fibonacci.rs
+++ examples/fibonacci.rs
@@ -1,59 +1,55 @@
-extern crate square;
+extern crate arc;
 
-use square::*;
+use arc::*;
 
-fn recursive_view(value: i16) -> View {
-
+fn recursive_view(c: &Context, value: i16) -> View {
     let (direction, color) = if value % 2 == 0 {
         if value % 4 == 0 {
-            (FlexDirection::Row, 0xa9d2f0)
+            (FlexDirection::Row, 0xff_a9d2f0)
         } else {
-            (FlexDirection::RowReverse, 0xa8ecc2)
+            (FlexDirection::RowReverse, 0xff_a8ecc2)
         }
     } else {
         if (value - 1) % 4 == 0 {
-            (FlexDirection::ColumnReverse, 0xf3c99f)
+            (FlexDirection::ColumnReverse, 0xff_f3c99f)
         }else {
-            (FlexDirection::Column, 0xf5b3ac)
+            (FlexDirection::Column, 0xff_f5b3ac)
         }
     };
 
-    let mut root = View::new();
-    root.set_flex_direction(direction);
-
-    let mut view = View::new();
-
+    let mut view = View::new(c);
     view.set_flex_grow(8.);
     view.set_background_color(color);
-
     view.set_margin(Edge::All, 1.5);
 
-    root.add_child(view);
+    let mut root = View::new(c);
+    root.set_flex_direction(direction);
+    root.add(view);
 
     let mut bview = if value != 0 {
-        recursive_view(value - 1)
+        recursive_view(c, value - 1)
     } else {
-        let mut v = View::new();
-        v.set_background_color(0xf8e896);
+        let mut v = View::new(c);
+        v.set_background_color(0xff_f8e896);
         return v;
     };
 
     bview.set_flex_grow(5.);
 
-    root.add_child(bview);
+    root.add(bview);
     root
 }
 
 
 fn main() {
-    let app = Application::new();
+    let c = Context::new();
 
-    let mut window = Window::new(1280., 800.);
+    let mut window = Window::new(&c, 1280., 800.);
     window.set_title("Fibonacci-ish");
-    window.set_view(recursive_view(12));
+    window.set_view(recursive_view(&c, 12));
     window.set_background_color(0xa9b2bb);
 
-    app.add_window(window);
+    window.show();
 
-    app.run();
+    c.run();
 }
